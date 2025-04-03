e663da9b300ac0749fd2f37598158e75f040cc86
devansh <devanshverma193@gmail.com>
2025-04-03 23:33:36 +0530

    tRPC

--- drizzle/0002_lonely_the_enforcers.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "users" DROP CONSTRAINT "users_github_id_unique";--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN "github_id";--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN "profile_url";--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN "location";--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN "github_created_at";
--- drizzle/0003_real_kree.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "users" ALTER COLUMN "user_name" DROP NOT NULL;
